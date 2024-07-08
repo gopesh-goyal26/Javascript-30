8c2726f0320b4e623e60a334b8e7bad3d55f6d9f
gopesh-goyal26 <gopeshgoyal26@gmail.com>
2024-07-08 22:25:51 +0530

    Day 26 Project

---
+++ 26 - Stripe Follow Along Nav/script.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector(".top");
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+
+function handleEnter(){
+    this.classList.add("trigger-enter");
+    setTimeout(() => this.classList.contains("trigger-enter") && this.classList.add("trigger-enter-active"), 1);
+    background.classList.add("open")
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    const coords = {
+        width: dropdownCoords.width,
+        height: dropdownCoords.height,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+    }
+
+    Object.keys(coords).forEach(key => background.style[key] = `${coords[key]}px`);
+
+}
+
+function handleLeave(){
+    this.classList.remove("trigger-enter-active");
+    setTimeout(() => !this.classList.contains("trigger-enter-active") && this.classList.remove("trigger-enter"), 300);
+    background.classList.remove("open");
+}
+
+triggers.forEach(trigger => {
+    trigger.addEventListener('mouseenter', handleEnter);
+    trigger.addEventListener('mouseleave', handleLeave)
+})
